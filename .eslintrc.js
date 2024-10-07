8f0583783669e641e34e8051aa083ed22f24affd
Alef A. Araujo <3alyef.dev@gmail.com>
2024-10-07 15:21:36 -0300

    fix: correacao .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,34 +4,33 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-native/all",
-    "prettier", // Para desativar regras conflitantes entre ESLint e Prettier
-    "plugin:prettier/recommended", // Ativa a integração do Prettier
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-native/all',
+    'prettier', // Para desativar regras conflitantes entre ESLint e Prettier
+    'plugin:prettier/recommended', // Ativa a integração do Prettier
   ],
-  parser: "@typescript-eslint/parser", // Definir o parser do TypeScript
+  parser: '@typescript-eslint/parser', // Definir o parser do TypeScript
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: "module",
+    sourceType: 'module',
   },
-  plugins: ["react", "react-native", "@typescript-eslint", "prettier"],
+  plugins: ['react', 'react-native', '@typescript-eslint', 'prettier'],
   rules: {
-    "prettier/prettier": ["error", {}, { usePrettierrc: true }], // Aplica as regras do prettier
-    "prettier/prettier": "error", // Exibe erros do Prettier no ESLint
-    "@typescript-eslint/explicit-module-boundary-types": "off", // Exemplo de ajuste para TypeScript
-    "react/react-in-jsx-scope": "off", // Desnecessário no React 17+
-    "react-native/no-inline-styles": "off",
-    "react-native/no-color-literals": "off", // Permitir literais de cor
-    "react-native/sort-styles": "error", // Forçar a classificação de propriedades de estilo
+    'prettier/prettier': ['warn', {}, { usePrettierrc: true }], // Aplica as regras do prettier
+    '@typescript-eslint/explicit-module-boundary-types': 'off', // Exemplo de ajuste para TypeScript
+    'react/react-in-jsx-scope': 'off', // Desnecessário no React 17+
+    'react-native/no-inline-styles': 'off',
+    'react-native/no-color-literals': 'off', // Permitir literais de cor
+    'react-native/sort-styles': 'off', // Forçar a classificação de propriedades de estilo
   },
   settings: {
     react: {
-      version: "detect",
+      version: 'detect',
     },
   },
 };
